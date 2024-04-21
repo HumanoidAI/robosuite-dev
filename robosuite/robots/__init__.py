f136daeb08e03bdec76709825f595a91e8aef407
Zhenyu Jiang <stevetod98@gmail.com>
2024-04-21 13:58:01 -0500

    init

---
+++ robosuite/robots/__init__.py
@@ -0,0 +1,35 @@
+from .fixed_base_robot import FixedBaseRobot
+from .mobile_base_robot import MobileBaseRobot
+from .wheeled_robot import WheeledRobot
+from .legged_robot import LeggedRobot
+
+from robosuite.models.robots.robot_model import REGISTERED_ROBOTS
+
+ALL_ROBOTS = REGISTERED_ROBOTS.keys()
+
+# Robot class mappings -- must be maintained manually
+ROBOT_CLASS_MAPPING = {
+    "Baxter": FixedBaseRobot,
+    "IIWA": FixedBaseRobot,
+    "Jaco": FixedBaseRobot,
+    "Kinova3": FixedBaseRobot,
+    "Panda": FixedBaseRobot,
+    "Sawyer": FixedBaseRobot,
+    "UR5e": FixedBaseRobot,
+    "VX300S": FixedBaseRobot,
+    "Z1": FixedBaseRobot,
+    "PandaMobile": WheeledRobot,
+    "GoogleRobot": WheeledRobot,
+    "VX300SMobile": WheeledRobot,
+    "Tiago": WheeledRobot,
+    "B1Z1": LeggedRobot,
+    # "B1Z1Floating": LeggedRobot,
+    "GR1": LeggedRobot,
+    "GR1FixedLowerBody": LeggedRobot,
+    "GR1ArmsOnly": LeggedRobot,
+    "GR1FloatingBody": LeggedRobot,
+    "H1": LeggedRobot,
+    "H1FixedLowerBody": LeggedRobot,
+    "H1FloatingBody": LeggedRobot,
+    "H1ArmsOnly": LeggedRobot,
+}
